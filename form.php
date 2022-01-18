689bdfec316a9403b8258b4d5ca1c807a144e729
Nicolas Chamorro <nichagiro@gmail.com>
2022-01-18 16:39:07 -0500

    php encode full

--- form.php
+++ form.php
@@ -1,3 +1,12 @@
 <?php
 
 return json_encode("Enviado");
+
+return json_encode("Enviado");
+
+return json_encode("Enviado");
+return json_encode("Enviado");
+return json_encode("Enviado");
+return json_encode("Enviado");
+return json_encode("Enviado");
+return json_encode("Enviado");
